59d5c888ff1c688544bc91b35fe86ea1cb978e6a
daviddesancho <daviddesancho@gmail.com>
2016-10-21 11:58:12 +0200

    tests : more exclusion

--- repeatdesigner/tests/test_basic.py
+++ repeatdesigner/tests/test_basic.py
@@ -1,25 +1,30 @@
+import os
 import unittest
 from repeatdesigner import designer as rd
 import warnings
 from Bio.PDB.PDBExceptions import PDBConstructionWarning
 warnings.simplefilter('ignore', PDBConstructionWarning)
 
+HERE = os.path.dirname(__file__)
+DATA_DIR = os.path.join(HERE, 'pdbs')
+filename = os.path.join(DATA_DIR,"1vii.pdb")
+
 
 class TestBasic(unittest.TestCase):
     def test_pdb(self):
-        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
-        self.assertEqual(villin_des.pdb, "repeatdesigner/tests/pdbs/1vii.pdb")
+        villin_des = rd.Design(pdb=filename)
+        self.assertEqual(villin_des.pdb, filename)
 
     def test_name(self):
-        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
-        self.assertEqual(villin_des.name, "repeatdesigner/tests/pdbs/1vii")
+        villin_des = rd.Design(pdb=filename)
+        self.assertEqual(villin_des.name, os.path.join(DATA_DIR,"1vii"))
 
     def test_sequence(self):
-        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
+        villin_des = rd.Design(pdb=filename)
         self.assertEqual(villin_des.seq, "MLSDEDFKAVFGMTRSAFANLPLWKQQNLKKEKGLF")
 
     def test_targets(self):
-        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
+        villin_des = rd.Design(filename)
         self.assertEqual(villin_des.targets, range(36))
 
 if __name__ == '__main__':
--- repeatdesigner/tests/test_repeat.py
+++ repeatdesigner/tests/test_repeat.py
@@ -1,14 +1,19 @@
+import os
 import unittest
 from repeatdesigner import designer as rd
 
+HERE = os.path.dirname(__file__)
+DATA_DIR = os.path.join(HERE, 'pdbs')
+filename = os.path.join(DATA_DIR,"3atb.pdb")
+
 class TestRepeatBasic(unittest.TestCase):
     def test_pdb(self):
-        tpr_des = rd.Design(pdb="repeatdesigner/tests/pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.pdb, "repeatdesigner/tests/pdbs/3atb.pdb")
+        tpr_des = rd.Design(pdb=filename)
+        self.assertEqual(tpr_des.pdb, filename)
 
     def test_name(self):
-        tpr_des = rd.Design(pdb="repeatdesigner/tests/pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.name, "repeatdesigner/tests/pdbs/3atb")
+        tpr_des = rd.Design(pdb=filename)
+        self.assertEqual(tpr_des.name, os.path.join(DATA_DIR,"3atb"))
 
     def test_sequence(self):
         tpr_des = rd.Design(pdb="repeatdesigner/tests/pdbs/3atb.pdb")
@@ -17,6 +22,8 @@ class TestRepeatBasic(unittest.TestCase):
     def test_targets(self):
         tpr_des = rd.Design(pdb="repeatdesigner/tests/pdbs/3atb.pdb")
         self.assertEqual(tpr_des.targets, range(119))
+
+#    def test_repeats(self):
 
 if __name__ == '__main__':
     unittest.main()

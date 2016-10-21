ababdc08c28b23ece6fee9756cc98e9e01c7b52b
daviddesancho <daviddesancho@gmail.com>
2016-10-21 11:39:28 +0200

    test : fixing locations

--- repeatdesigner/tests/test_basic.py
+++ repeatdesigner/tests/test_basic.py
@@ -7,19 +7,19 @@ warnings.simplefilter('ignore', PDBConstructionWarning)
 
 class TestBasic(unittest.TestCase):
     def test_pdb(self):
-        villin_des = rd.Design(pdb="pdbs/1vii.pdb")
-        self.assertEqual(villin_des.pdb, "pdbs/1vii.pdb")
+        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
+        self.assertEqual(villin_des.pdb, "repeatdesigner/tests/pdbs/1vii.pdb")
 
     def test_name(self):
-        villin_des = rd.Design(pdb="pdbs/1vii.pdb")
-        self.assertEqual(villin_des.name, "pdbs/1vii")
+        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
+        self.assertEqual(villin_des.name, "repeatdesigner/tests/pdbs/1vii")
 
     def test_sequence(self):
-        villin_des = rd.Design(pdb="pdbs/1vii.pdb")
+        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
         self.assertEqual(villin_des.seq, "MLSDEDFKAVFGMTRSAFANLPLWKQQNLKKEKGLF")
 
     def test_targets(self):
-        villin_des = rd.Design(pdb="pdbs/1vii.pdb")
+        villin_des = rd.Design(pdb="repeatdesigner/tests/pdbs/1vii.pdb")
         self.assertEqual(villin_des.targets, range(36))
 
 if __name__ == '__main__':

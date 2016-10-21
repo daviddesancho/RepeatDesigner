b92156936a00bc613da955fe89f68a19cfa2c81b
daviddesancho <daviddesancho@gmail.com>
2016-10-21 10:58:44 +0200

    test : fixed PDBConstructionWarning

--- repeatdesigner/tests/test_basic.py
+++ repeatdesigner/tests/test_basic.py
@@ -1,5 +1,9 @@
 import unittest
 from repeatdesigner import designer as rd
+import warnings
+from Bio.PDB.PDBExceptions import PDBConstructionWarning
+warnings.simplefilter('ignore', PDBConstructionWarning)
+
 
 class TestBasic(unittest.TestCase):
     def test_pdb(self):
@@ -17,22 +21,6 @@ class TestBasic(unittest.TestCase):
     def test_targets(self):
         villin_des = rd.Design(pdb="pdbs/1vii.pdb")
         self.assertEqual(villin_des.targets, range(36))
-
-class TestRepeatBasic(unittest.TestCase):
-    def test_pdb(self):
-        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.pdb, "pdbs/3atb.pdb")
-
-    def test_name(self):
-        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.name, "pdbs/3atb")
-
-    def test_sequence(self):
-        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.seq, "NSAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAKQDLGNAKQKQG")
-    def test_targets(self):
-        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
-        self.assertEqual(tpr_des.targets, range(119))
 
 if __name__ == '__main__':
     unittest.main()
---
+++ repeatdesigner/tests/test_repeat.py
@@ -0,0 +1,22 @@
+import unittest
+from repeatdesigner import designer as rd
+
+class TestRepeatBasic(unittest.TestCase):
+    def test_pdb(self):
+        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
+        self.assertEqual(tpr_des.pdb, "pdbs/3atb.pdb")
+
+    def test_name(self):
+        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
+        self.assertEqual(tpr_des.name, "pdbs/3atb")
+
+    def test_sequence(self):
+        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
+        self.assertEqual(tpr_des.seq, "NSAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAWYNLGNAYYKQGDYDEAIEYYQKALELDPNNAEAKQDLGNAKQKQG")
+
+    def test_targets(self):
+        tpr_des = rd.Design(pdb="pdbs/3atb.pdb")
+        self.assertEqual(tpr_des.targets, range(119))
+
+if __name__ == '__main__':
+    unittest.main()
